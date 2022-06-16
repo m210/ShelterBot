b17111b8a84d50ee39aa2739e63c9a3875b7fb31
[M210®] <m210-2007@mail.ru>
2022-06-16 19:52:36 +0300

    Initial commit

---
+++ src/main/java/sky/pro/shelterbot/listener/BotUpdatesListener.java
@@ -0,0 +1,43 @@
+package sky.pro.shelterbot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class BotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(BotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+
+            Message message = update.message();
+            if (message == null) {
+                return;
+            }
+        });
+
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
+
